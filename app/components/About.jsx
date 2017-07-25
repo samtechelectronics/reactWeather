72fbce668d5d89c97ac841a3ce7fea9d3d2d379b
samuel <samueleke71@gmail.com>
2017-07-25 13:17:22 +0100

    update now

---
+++ app/components/About.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+/*var About = React.createClass({
+  render: function () {
+    return (
+      <h2>About page </h2>
+    );
+  }
+
+}); */
+var About = (props) => {
+  return (
+    <h2>About page </h2>
+  );
+}
+module.exports = About;
